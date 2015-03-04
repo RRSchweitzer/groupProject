2e7fb807781a0c5dd689eb047cbe48550062e502
Ty Frith <tyfrith@gmail.com>
2015-03-04 14:12:16 -0700

    werk

--- Public/js/controllers/homeCtrl.js
+++ Public/js/controllers/homeCtrl.js
@@ -2,4 +2,19 @@ var app = angular.module('group');
 
 app.controller('homeCtrl', function($scope) {
 	
+//menu dropdown in index.hmtl js
+	$scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+//----- end dropdown 
 })
