8a4b5cc2f2bc9856c62e29e0d6e49b61376318bf
Ryan <RR.Schweitzer@gmail.com>
2015-03-18 10:41:17 -0600

    stuff

--- Public/js/controllers/bootcampCtrl.js
+++ Public/js/controllers/bootcampCtrl.js
@@ -1,5 +1,15 @@
 app = angular.module('group');
 
 app.controller('bootcampCtrl', function($scope) {
-	
+	$scope.list1 = {title: 'User Name'};
+  $scope.list2 = {};
+
+  $scope.getUnverifiedUsers = function() {
+		bootcampService.getUnverifiedUsers()
+		.then(function(res) {
+			$scope.unverifiedUsers = res.data;
+		})
+	}
+	$scope.getProjects();
+
 })
