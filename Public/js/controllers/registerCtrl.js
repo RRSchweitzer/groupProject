24462aa1b804237428170eb9e6587fabff49a610
Aodhan Hayter <aodhan.hayter@gmail.com>
2015-03-19 14:06:58 -0600

    populate working

--- Public/js/controllers/registerCtrl.js
+++ Public/js/controllers/registerCtrl.js
@@ -2,18 +2,14 @@ var app = angular.module('group');
 
 
 app.controller('registerCtrl', function($scope, $log, $modal, $location, registerService) {
-<<<<<<< HEAD
-	 	// $scope.bootcamps = registerService.getBootcamps().then(function(res) {
-	 	// 	return res.data
-	 	// })
-=======
+
 		$scope.bootcamps = registerService.getBootcamps()
 			.then(function(res) {
 				console.log("this is res.data")
 				console.log(res.data)
 				return res.data
 			})
->>>>>>> upstream/master
+
 		$scope.user = registerService.getUser().then(function (res) {
 			return res.data;
 		})
@@ -29,11 +25,7 @@ app.controller('registerCtrl', function($scope, $log, $modal, $location, registe
 					return $scope.user;
 				},
 				bootcampsObj: function () {
-<<<<<<< HEAD
 					return $scope.bootcamps;
-=======
-					return $scope.bootcamps
->>>>>>> upstream/master
 				}
 			}
 		})
